56a1f788d1c85117b631eac61a9b37708b69becf
alexs <alexplus77@mail.ru>
2021-12-25 20:16:48 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import HomePage from "pages/HomePage";
-import DriftPage from "pages/DriftPage";
+import { Router } from "./Routes";
 import Menu from "pages/Menu";
-import ForzaPage from "pages/ForzaPage";
-import TimeAttackPage from "pages/TimeAttackPage";
+
 import "index.css";
 export default function App() {
   return (
     <div>
       <Menu />
-      <div className="page">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/drift" element={<DriftPage />} />
-          <Route path="/timeattack" element={<TimeAttackPage />} />
-          <Route path="/forza" element={<ForzaPage />} />
-        </Routes>
-      </div>
+      <Router />
     </div>
   );
 }
---
+++ src/Routes.js
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import DriftPage from "./pages/DriftPage";
+import TimeAttackPage from "./pages/TimeAttackPage";
+import ForzaPage from "./pages/ForzaPage";
+
+const Router = () => {
+  return (
+    <div className="page">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/drift" element={<DriftPage />} />
+        <Route path="/timeattack" element={<TimeAttackPage />} />
+        <Route path="/forza" element={<ForzaPage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export { Router };
